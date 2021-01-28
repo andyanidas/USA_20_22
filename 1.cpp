8cb3dd1f3b81767925ff1d04739a3daabc735a84
Lkhagva-erdene <anidas.com@gmail.com>
2021-01-28 06:05:42 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello andy"<<endl;
+	cout<<"Hello andy"<<endl;
+	cout<<"sssss andy"<<endl;
+	cout<<"Hello andy";
+	
+	
+	
+	return 0;
+}
